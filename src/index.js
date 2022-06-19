8b949c55302678646e88af4b87f78b88681f29da
Frangit <leandro.frangit@gmail.com>
2022-06-19 11:56:49 -0300

    Testes 19/06
    alguns testes para adicionar novos comandos e congifurar o circle

--- src/index.js
+++ src/index.js
@@ -1,40 +1,64 @@
+const { Routes } = require('discord-api-types/v9');
+const { REST } = require('@discordjs/rest');
+
 // Require the necessary discord.js classes
 const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
-require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Import dos comandos
-client.commands = new Collection();
+const commands = [];
 const commandFiles = fs.readdirSync('src/commands').filter(file => file.endsWith('.js'));
+
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+	commands.push(command.data.toJSON());
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+			{ body: commands },
+		)
+			.then(console.log('Successfully reloaded application (/) commands.'))
+			.catch(console.error);
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
+
+
+const eventFiles = fs.readdirSync('src/events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
 	}
 });
 
