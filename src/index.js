8d6be0b2faed990e0d951b8d10b901b6cfc2c473
Frangit <leandro.frangit@gmail.com>
2022-06-19 20:15:29 -0300

    fix: retirando code smell

--- src/index.js
+++ src/index.js
@@ -36,23 +36,5 @@ for (const file of eventFiles) {
 	}
 }
 
-/*
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-*/
-
 // Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
